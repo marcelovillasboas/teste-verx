40bea57413e1f7c194de034d39d76dc8e1e619fc
marcelovillasboas <marcelovillasboas@outlook.com>
2024-01-05 22:43:13 -0300

    remove comments

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,6 @@ const PORT = process.env.PORT || 3000
 app.use(bodyParser.json())
 app.use('/api', producerRoutes)
 
-// Add other middleware, routes, and configurations as needed
-
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
